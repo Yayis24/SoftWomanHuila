83eb3889ef236c1f6c0927d8caf81aed136fb63b
Aldana2005 <mary.aldana022@gmail.com>
2023-09-27 16:00:14 -0500

    Se creo modal del formulario de solicitud de servicio

--- resources/views/solicituddeservicio.blade.php
+++ resources/views/solicituddeservicio.blade.php
@@ -2,39 +2,26 @@
 <html>
 
 <head>
-    <title>Solicitud Servicio</title>
+    <meta charset="UTF-8">
+    <title>Solicitud de Servicio</title>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/solicitudservicio.css') }}">
 </head>
 
-<body>
+<body class="body">
     <h1 class="map-title">¡Encuentra Tu Manzana Más Cercana!</h1>
-    <div class="mapouter">
-        <div class="gmap_canvas"><iframe width="820" height="560" id="gmap_canvas"
+    <div class="map-container">
+        <div class="gmap_canvas">
+            <iframe width="820" height="560" id="gmap_canvas"
                 src="https://maps.google.com/maps?q=Localidades+con+Manzanas+Activas&t=k&z=13&ie=UTF8&iwloc=&output=embed"
-                frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a
-                href="https://textcaseconvert.com/"></a><br><a href="https://online-timer.me/"></a><br>
-           {{--  <style>
-                .mapouter {
-                    position: relative;
-                    text-align: right;
-                    height: 560px;
-                    width: 820px;
-                }
-            </style><a href="https://www.embedmaps.co">create map in google</a>
-            <style>
-                .gmap_canvas {
-                    overflow: hidden;
-                    background: none !important;
-                    height: 560px;
-                    width: 820px;
-                }
-            </style> --}}
+                frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
         </div>
     </div>
-    </div>
-    </div>
+
     <h1 class="text-center">Solicitud de Servicio</h1>
-    <table>
+    <button class="my-button" onclick="openModal()">Agregar Solicitud</button>
+    <table id="solicitudTable" class="display">
         <thead>
             <tr>
                 <th>ID</th>
@@ -50,76 +37,94 @@
             <tr>
                 <td>1</td>
                 <td class="manzana" data-id="1">Azul</td>
-                <td>Educacion superior</td>
+                <td>Educación superior</td>
                 <td>7/01/2024</td>
                 <td>Aprobada</td>
                 <td>Calle 3A-14-9</td>
                 <td>
-                    <button class="my-button">Agregar</button>
                     <button class="my-button1">Eliminar</button>
                 </td>
             </tr>
-            <!-- Puedes agregar más filas con datos aquí -->
+            <!-- Agrega más filas según sea necesario -->
         </tbody>
     </table>
 
-    <!-- Tarjeta informativa -->
-    <div class="tarjeta" id="tarjeta">
-        <!-- Contenido de la tarjeta -->
-        <h2>Servicio: Centro Día Carlos Gaviria Díaz</h2>
-        <p>DIRECCIÓN: Calle 69 Sur #18b - 36</p>
-        <p>Horario: Lunes a Viernes de 08:00 am a 5:00 pm</p>
-        <p>TELEFONO: 6,012,830,541</p>
-        <p>LOCALIDAD: Ciudad Bolívar</p>
-        <p>MANZANA: Manitas</p>
-        <p>TIPO_SERVI: Para quienes requieren Cuidado</p>
-        <p>Equipamiento: Centro Día Carlos Gaviria Díaz</p>
+    <div id="modal" class="modal">
+        <div class="modal-contenido">
+            <i class="fas fa-times cerrar" onclick="closeModal()"></i>
+            <h2>Agregar Solicitud de Servicio</h2>
+            <form id="formularioAgregar">
+                <label for="manzana">Manzana:</label>
+                <input type="text" id="manzana" name="manzana" required>
+                <label for="servicio">Servicio:</label>
+                <input type="text" id="servicio" name="servicio" required>
+                <label for="fecha">Fecha/Hora:</label>
+                <input type="text" id="fecha" name="fecha" required>
+                <label for="estado">Estado:</label>
+                <input type="text" id="estado" name="estado" required>
+                <label for="observacion">Observación:</label>
+                <input type="text" id="observacion" name="observacion">
+                <button type="submit" class="my-button">Guardar</button>
+            </form>
+        </div>
     </div>
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+
     <script>
-        // Obtener las manzanas y la tarjeta
-        const manzanas = document.querySelectorAll('.manzana');
-        const tarjeta = document.getElementById('tarjeta');
+        function openModal() {
+            document.getElementById('modal').style.display = 'block';
+        }
 
-        // Agregar un controlador de eventos de clic a cada manzana
-        manzanas.forEach(manzana => {
-            manzana.addEventListener('click', () => {
-                // Obtener el ID de la manzana seleccionada (en este ejemplo, usamos el atributo data-id)
-                const manzanaId = manzana.getAttribute('data-id');
-                
-                // Simular una solicitud de datos basada en el ID de la manzana
-                // (reemplaza esto con tu lógica real de obtención de datos)
-                // Esto es solo un ejemplo para mostrar la tarjeta
-                // Cuando se obtengan los datos reales, puedes actualizar el contenido de la tarjeta
-                const datosManzana = {
-                    servicio: "Centro Día Carlos Gaviria Díaz",
-                    direccion: "Calle 69 Sur #18b - 36",
-                    horario: "Lunes a Viernes de 08:00 am a 5:00 pm",
-                    telefono: "6,012,830,541",
-                    localidad: "Ciudad Bolívar",
-                    manzana: "Manitas",
-                    tipoServicio: "Para quienes requieren Cuidado",
-                    equipamiento: "Centro Día Carlos Gaviria Díaz"
-                };
+        function closeModal() {
+            document.getElementById('modal').style.display = 'none';
+        }
 
-                // Actualizar el contenido de la tarjeta con los datos de la manzana
-                tarjeta.innerHTML = `
-                    <h2>Servicio: ${datosManzana.servicio}</h2>
-                    <p>DIRECCIÓN: ${datosManzana.direccion}</p>
-                    <p>Horario: ${datosManzana.horario}</p>
-                    <p>TELEFONO: ${datosManzana.telefono}</p>
-                    <p>LOCALIDAD: ${datosManzana.localidad}</p>
-                    <p>MANZANA: ${datosManzana.manzana}</p>
-                    <p>TIPO_SERVI: ${datosManzana.tipoServicio}</p>
-                    <p>Equipamiento: ${datosManzana.equipamiento}</p>
-                `;
+        document.getElementById('formularioAgregar').addEventListener('submit', function (event) {
+            event.preventDefault();
+            // Aquí puedes procesar los datos del formulario y guardar la solicitud
+            // Por ejemplo, puedes usar AJAX para enviar los datos al servidor
+            // Después de guardar la solicitud, cierra el modal
+            closeModal();
+        });
 
-                // Mostrar la tarjeta
-                tarjeta.style.display = 'block';
+        $(document).ready(function () {
+            $('#solicitudTable').DataTable({
+                responsive: true,
+                dom: 'Bfrtip',
+                buttons: [
+                    {
+                        extend: 'excelHtml5',
+                        title: 'Solicitud de Servicio List',
+                    },
+                    {
+                        extend: 'pdfHtml5',
+                        title: 'Solicitud de Servicio List',
+                        exportOptions: {
+                            columns: ':visible'
+                        },
+                        customize: function (doc) {
+                            // Personaliza el documento PDF si es necesario
+                        }
+                    },
+                    {
+                        extend: 'print',
+                        title: 'Solicitud de Servicio List',
+                    },
+                    {
+                        extend: 'copy',
+                        title: 'Solicitud de Servicio List',
+                    }
+                ]
             });
         });
-    </script> 
+    </script>
 </body>
 
 </html>
-
